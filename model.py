2bb188fa9ede4feeba3aa0195f63e288c8b79b87
vcrbeta <vcrjames@gmail.com>
2025-04-18 11:04:53 -0400

    Initial commit

--- model.py
+++ model.py
@@ -11,12 +11,14 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
-
+    
+    ratings = db.relationship("Rating", back_populates="user")
+    
     def __repr__(self):
-        return f"<User id={self.id} email={self.email}>"
+        return f"<User user_id={self.user_id} email={self.email}>"
 
 class Movie(db.Model):
     """A movie."""
@@ -28,7 +30,9 @@ class Movie(db.Model):
     overview = db.Column(db.Text)  
     release_date = db.Column(db.DateTime) 
     poster_path = db.Column(db.String) 
-
+    
+    ratings = db.relationship("Rating", back_populates="movie")
+    
     def __repr__(self):
         return f'<Movie movie_id={self.movie_id} title={self.title} release_year={self.release_year}>'
 
@@ -37,21 +41,16 @@ class Rating(db.Model):
 
     __tablename__ = 'ratings'
 
-    rating_id = db.Column(db.Integer, 
-                          primary_key=True, 
-                          autoincrement=True)
-    
+    rating_id = db.Column(db.Integer,primary_key=True, autoincrement=True)
     score = db.Column(db.Integer)
-    
-    movie_id = db.Column(db.Integer, 
-                         db.ForeignKey('movies.movie_id'))  # create relationship later
-    
-    user_id = db.Column(db.Integer, 
-                        db.ForeignKey('users.user_id'))  # create relationship later
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))  
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))  
 
     def __repr__(self):
         return f"<Rating rating_id={self.rating_id} score={self.score}>"
-
+    
+    movie = db.relationship("Movie", back_populates="ratings")
+    user = db.relationship("User", back_populates="ratings")
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
